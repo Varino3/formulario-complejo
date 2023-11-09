4f9d0465e923ef8d07762fdcdf0b61fdd4e73c73
Alvaro Contreras <contreras1999@gmail.com>
2023-11-09 10:02:06 +0100

    mostrar datos pantalla

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
-import React from "react";
-import Form from "./Form";
+import React, { useState } from "react";
+import Form from "./components/Form";
+import MostrarDatos from "./components/MostrarDatos";
+import './App.css'
 
 function App() {
+  const [formData, setFormData] = useState({
+    selectedItems: [],
+    email: "",
+    age: 0,
+    option: "none",
+    subOption1: "A",
+    subOption2: "X",
+  });
+
+  const [showData, setShowData] = useState(false);
+
+  const handleFormSubmit = (data) => {
+    setFormData(data);
+    setShowData(true);
+  };
+
+  const handleReset = () => {
+    setShowData(false);
+  };
+
   return (
     <div>
-      <Form />
+      {!showData ? (
+        <Form onSubmit={handleFormSubmit} />
+      ) : (
+        <div>
+          <MostrarDatos formData={formData} />
+          <button onClick={handleReset}>Volver al formulario</button>
+        </div>
+      )}
     </div>
   );
 }
